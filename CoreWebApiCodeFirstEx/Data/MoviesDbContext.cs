6ffeef32e7dc3c0ccaa792dbd049b2a315e3231b
Ritesh0610 <yadavitin197@gmail.com>
2021-03-17 15:51:38 +0530

    After Completing the task

---
+++ CoreWebApiCodeFirstEx/Data/MoviesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CoreWebApiCodeFirstEx.Models;
+
+namespace CoreWebApiCodeFirstEx.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext (DbContextOptions<MoviesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CoreWebApiCodeFirstEx.Models.Movie> Movie { get; set; }
+    }
+}
